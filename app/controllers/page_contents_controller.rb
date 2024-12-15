ddfbf4dc0180ce8aff7ea21b6eea821cd04a8a4f
HiroyukiKano <kano@netlab.jp>
2024-12-15 15:30:26 +0900

    ページ編集画面のフラッシュメッセージの改修

--- app/controllers/page_contents_controller.rb
+++ app/controllers/page_contents_controller.rb
@@ -2,15 +2,15 @@ class PageContentsController < ApplicationController
   def update
     @page_content = PageContent.find(page_contents_params[:page_content_id])
     if @page_content.update(contents: page_contents_params[:contents].split(","))
-      flash[:success] = "更新しました。"
+      flash[page_contents_params[:head] + "成功"] = "更新しました。"
     else
-      flash[:danger] = @page_content.errors.full_messages.join(" / ")
+      flash[page_contents_params[:head] + "失敗"] = @page_content.errors.full_messages.join(" / ")
     end
-    redirect_to root_path
+    redirect_to ship_captain_page_edit_path
   end
 
   private
   def page_contents_params
-    params.permit(:page_content_id, :contents)
+    params.permit(:page_content_id, :head, :contents)
   end
 end
--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -3,7 +3,22 @@ class PageContent < ApplicationRecord
 
   validates :display_page, presence: true
   validates :head, presence: true, length: { maximum: 20 }
-  validates :contents, length: { maximum: 150 }
+  validates :contents, length: { maximum: 5 }
+
+  validate :contents_text_length
+
+  def contents_text_length
+    return if contents.empty?
+
+    invalid_indexs = []
+    contents.each_with_index do |content, i|
+      invalid_indexs << i+1 if content.length > 10
+    end
+
+    if invalid_indexs.length > 0
+      errors.add(:contents, ": 10字を超える項目(#{invalid_indexs.join(', ')}項目目)は設定できません。")
+    end
+  end
 
   enum :display_page, { home: 0, menu: 1, ship: 2, safety: 3, schedule: 4, link: 5 }
 end
