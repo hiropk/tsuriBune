ace97962be998b6aaf69eb695de29fd73188b973
HiroyukiKano <kano@netlab.jp>
2024-12-25 14:28:36 +0900

    .

--- db/migrate/20241225052453_create_fishing_service_infos.rb
+++ db/migrate/20241225052453_create_fishing_service_infos.rb
@@ -1,16 +1,16 @@
 class CreateFishingServiceInfos < ActiveRecord::Migration[8.0]
   def change
     create_table :fishing_service_infos do |t|
-      t.string :name
-      t.integer :pref
-      t.integer :number
-      t.date :registration_start_date
-      t.date :registration_end_date
-      t.string :address
-      t.string :ship_name
-      t.text :administrator
-      t.date :insurance_start_date
-      t.date :insurance_end_date
+      t.string :name, null: false
+      t.integer :pref, null: false
+      t.integer :number, null: false
+      t.date :registration_start_date, null: false
+      t.date :registration_end_date, null: false
+      t.string :address, null: false
+      t.string :ship_name, null: false
+      t.text :administrator, null: false
+      t.date :insurance_start_date, null: false
+      t.date :insurance_end_date, null: false
 
       t.timestamps
     end
