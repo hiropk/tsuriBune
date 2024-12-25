a071f3fd94a23b2a441343d2b246ea3e2212bf78
HiroyukiKano <kano@netlab.jp>
2024-12-25 10:59:25 +0900

    フラッシュメッセージの統一

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,69 +1,55 @@
 class MenusController < ApplicationController
   before_action :set_menu, only: %i[ show edit update destroy ]
 
-  # GET /menus or /menus.json
   def index
     @menus = Menu.all
   end
 
-  # GET /menus/1 or /menus/1.json
   def show
   end
 
-  # GET /menus/new
   def new
     @menu = Menu.new
   end
 
-  # GET /menus/1/edit
   def edit
   end
 
-  # POST /menus or /menus.json
   def create
     @menu = Menu.new(menu_params)
 
     respond_to do |format|
       if @menu.save
-        format.html { redirect_to @menu, notice: "Menu was successfully created." }
-        format.json { render :show, status: :created, location: @menu }
+        format.html { redirect_to @menu, notice: t("dictionary.message.create.complete") }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @menu.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /menus/1 or /menus/1.json
   def update
     respond_to do |format|
       if @menu.update(menu_params)
-        format.html { redirect_to @menu, notice: "Menu was successfully updated." }
-        format.json { render :show, status: :ok, location: @menu }
+        format.html { redirect_to @menu, notice: t("dictionary.message.update.complete") }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @menu.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /menus/1 or /menus/1.json
   def destroy
     @menu.destroy!
 
     respond_to do |format|
-      format.html { redirect_to menus_path, status: :see_other, notice: "Menu was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to menus_path, status: :see_other, notice: t("dictionary.message.destory.complete") }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_menu
       @menu = Menu.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def menu_params
       params.expect(menu: [ :name, :embarkation_type, :unit_price, :acceptable_num, :payment_method ])
     end
