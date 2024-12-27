a99d1b110eaa3e4cc9de6b218599c5a5cda6279a
HiroyukiKano <kano@netlab.jp>
2024-12-27 22:37:39 +0900

    遊漁船業者登録票のupdateを追加

--- app/controllers/fishing_service_infos_controller.rb
+++ app/controllers/fishing_service_infos_controller.rb
@@ -1,12 +1,31 @@
 class FishingServiceInfosController < ApplicationController
+  before_action :set_fishing_service_info, only: %i[ show edit update ]
   def show
-    @fishing_service_info = FishingServiceInfo.first
   end
 
   def edit
-    @fishing_service_info = FishingServiceInfo.first
   end
 
   def update
+    respond_to do |format|
+      if @fishing_service_info.update(fishing_service_info_params)
+        format.html { redirect_to @fishing_service_info, notice: t("dictionary.message.update.complete") }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def set_fishing_service_info
+    @fishing_service_info = FishingServiceInfo.find(params.expect(:id))
+  end
+
+  def fishing_service_info_params
+    if administrator_str = params[:fishing_service_info][:administrator]
+      params[:fishing_service_info][:administrator] = administrator_str.split(",")
+    end
+
+    params.expect(fishing_service_info: [ :name, :pref, :number, :registration_start_date, :registration_end_date, :address, :ship_name, :administrator ])
   end
 end
