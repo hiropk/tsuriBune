4a0e666f864ff4a19100b0ab1635a2a289e60648
HiroyukiKano <kano@netlab.jp>
2025-01-13 17:01:07 +0900

    スケジュールのindex周りの機能開発

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :menu
+
+  scope :on, ->(date) do
+    where(start_time: date.to_datetime..).where(start_time: ...(date.to_datetime + 1.day))
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_072627) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_13_073203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,7 +57,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_09_072627) do
 
   create_table "schedules", force: :cascade do |t|
     t.bigint "menu_id", null: false
-    t.integer "departure_type", null: false
     t.integer "num_reservation", default: 0, null: false
     t.text "memo"
     t.datetime "start_time", null: false
