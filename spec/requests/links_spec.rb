53f0a189d7f88896951c9370e87b98cb73ead90b
HiroyukiKano <kano@netlab.jp>
2025-02-12 16:09:37 +0900

    リンクのspecを追加

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -13,119 +13,146 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/links", type: :request do
-  
-  # This should return the minimal set of attributes required to create a valid
-  # Link. As you add validations to Link, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  describe "GET /index" do
-    it "renders a successful response" do
-      Link.create! valid_attributes
-      get links_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      link = Link.create! valid_attributes
-      get link_url(link)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_link_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "renders a successful response" do
-      link = Link.create! valid_attributes
-      get edit_link_url(link)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Link" do
-        expect {
-          post links_url, params: { link: valid_attributes }
-        }.to change(Link, :count).by(1)
-      end
-
-      it "redirects to the created link" do
-        post links_url, params: { link: valid_attributes }
-        expect(response).to redirect_to(link_url(Link.last))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "does not create a new Link" do
-        expect {
-          post links_url, params: { link: invalid_attributes }
-        }.to change(Link, :count).by(0)
-      end
-
-      it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post links_url, params: { link: invalid_attributes }
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-    end
-  end
-
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+  describe "ログイン済の場合" do
+    let(:valid_attributes) {
+      {
+        name: "リンクA",
+        url: "https://familiar.onrender.com/",
+        description: "遊漁船ファミリアのホームページ"
       }
+    }
 
-      it "updates the requested link" do
-        link = Link.create! valid_attributes
-        patch link_url(link), params: { link: new_attributes }
-        link.reload
-        skip("Add assertions for updated state")
-      end
+    let(:invalid_attributes) {
+      {
+        name: "リンクB",
+        url: "無効なURL",
+        description: "長すぎる説明"*30
+      }
+    }
 
-      it "redirects to the link" do
-        link = Link.create! valid_attributes
-        patch link_url(link), params: { link: new_attributes }
-        link.reload
-        expect(response).to redirect_to(link_url(link))
+    before do
+      user = create(:user, :admin, password: "password")
+      create(:fishing_service_info)
+      post session_url, params: { email_address: user.email_address, password: 'password' }
+    end
+
+    describe "GET /index" do
+      it "renders a successful response" do
+        Link.create! valid_attributes
+        get links_url
+        expect(response).to be_successful
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
+    describe "GET /show" do
+      it "renders a successful response" do
         link = Link.create! valid_attributes
-        patch link_url(link), params: { link: invalid_attributes }
-        expect(response).to have_http_status(:unprocessable_entity)
+        get link_url(link)
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET /new" do
+      it "renders a successful response" do
+        get new_link_url
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET /edit" do
+      it "renders a successful response" do
+        link = Link.create! valid_attributes
+        get edit_link_url(link)
+        expect(response).to be_successful
+      end
+    end
+
+    describe "POST /create" do
+      context "with valid parameters" do
+        it "creates a new Link" do
+          expect {
+            post links_url, params: { link: valid_attributes }
+          }.to change(Link, :count).by(1)
+        end
+
+        it "redirects to the created link" do
+          post links_url, params: { link: valid_attributes }
+          expect(response).to redirect_to(link_url(Link.last))
+        end
+      end
+
+      context "with invalid parameters" do
+        it "does not create a new Link" do
+          expect {
+            post links_url, params: { link: invalid_attributes }
+          }.to change(Link, :count).by(0)
+        end
+
+        it "renders a response with 422 status (i.e. to display the 'new' template)" do
+          post links_url, params: { link: invalid_attributes }
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    describe "PATCH /update" do
+      context "with valid parameters" do
+        let(:new_attributes) {
+          {
+            name: "リンクC",
+            url: "https://familiar.onrender.com/hello",
+            description: "遊漁船ファミリアのホームページを更新しました。"
+          }
+        }
+
+        it "updates the requested link" do
+          link = Link.create! valid_attributes
+          patch link_url(link), params: { link: new_attributes }
+          link.reload
+          expect(link.name).to eq "リンクC"
+          expect(link.url).to eq "https://familiar.onrender.com/hello"
+          expect(link.description).to eq "遊漁船ファミリアのホームページを更新しました。"
+        end
+
+        it "redirects to the link" do
+          link = Link.create! valid_attributes
+          patch link_url(link), params: { link: new_attributes }
+          link.reload
+          expect(response).to redirect_to(link_url(link))
+        end
+      end
+
+      context "with invalid parameters" do
+        it "renders a response with 422 status (i.e. to display the 'edit' template)" do
+          link = Link.create! valid_attributes
+          patch link_url(link), params: { link: invalid_attributes }
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    describe "DELETE /destroy" do
+      it "destroys the requested link" do
+        link = Link.create! valid_attributes
+        expect {
+          delete link_url(link)
+        }.to change(Link, :count).by(-1)
+      end
+
+      it "redirects to the links list" do
+        link = Link.create! valid_attributes
+        delete link_url(link)
+        expect(response).to redirect_to(links_url)
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested link" do
-      link = Link.create! valid_attributes
-      expect {
-        delete link_url(link)
-      }.to change(Link, :count).by(-1)
-    end
-
-    it "redirects to the links list" do
-      link = Link.create! valid_attributes
-      delete link_url(link)
-      expect(response).to redirect_to(links_url)
+  describe "未ログインの場合" do
+    context "メニュー編集画面を表示したとき" do
+      it "リダイレクトされること" do
+        get "/links"
+        expect(response).to have_http_status(302)
+      end
     end
   end
 end
